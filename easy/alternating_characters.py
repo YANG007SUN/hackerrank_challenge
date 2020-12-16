d2e7901ae58d5ec61c1ebd86223b9d44e9a568a8
yang sun <yangsun@yangs-MacBook-Pro.local>
2020-12-15 22:45:49 -0800

    updating

--- easy/alternating_characters.py
+++ easy/alternating_characters.py
@@ -4,7 +4,6 @@ def alternatingCharacters(s):
     c = 0
     for i in range(n-1,0,-1):
         if s[i]==s[i-1]:
-            print(i, s[i], s[i-1])
             del s[i]
             i-=1
             c+=1
